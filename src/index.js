1c74600b2aee2954d43377930024852575d2f73d
Yogi Delfiandra <yogi.delfiandra03@gmail.com>
2023-05-28 20:56:39 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Todo from './components/Todo';
+import ToggleLamp from './components/ToggleLamp';
+
+import './styles/style.css';
+
+function App() {
+  return (
+    <>
+      <h1>
+        Latihan <code>useState()</code>
+      </h1>
+      <section>
+        <h2>
+          Kasus 1: <code>ToggleLamp</code> Component
+        </h2>
+        <p>Nyalakan lampu untuk melihat pesan.</p>
+        <ToggleLamp />
+      </section>
+      <section>
+        <h2>
+          Kasus 2: <code>Todo</code> Component
+        </h2>
+        <Todo />
+      </section>
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +34,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
